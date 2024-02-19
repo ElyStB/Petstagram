7e954bbbfa769df08310260c8b06c464c35e684b
Elena Boneva <stojcheva_eli@abv.bg>
2024-02-19 07:59:56 +0200

    add create and edit pet functionality fix pet_details.html include (with pet_photo=pet.petphoto_set) no spaces

---
+++ petstagram/pets/forms.py
@@ -0,0 +1,31 @@
+from django import forms
+
+from petstagram.pets.models import Pet
+
+
+class PetBaseForm(forms.ModelForm):
+    class Meta:
+        model = Pet
+        fields = ['name', 'date_of_birth', 'pet_photo']
+        widgets = {
+            'name': forms.TextInput(attrs={'placeholder': 'Pet name'}),
+            'date_of_birth': forms.DateInput(attrs={'type': 'date'}),
+            'personal_photo': forms.URLInput(attrs={'placeholder': 'Link to image'}),
+        }
+        labels = {
+            'name': 'Pet Name',
+            'date_of_birth': 'Date of Birth',
+            'personal_photo': 'Link to Image',
+        }
+
+
+class PetCreateForm(PetBaseForm):
+    pass
+
+
+class PetEditForm(PetBaseForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['date_of_birth'].widget.attrs['readonly'] = 'readonly'
+
+
--- petstagram/pets/views.py
+++ petstagram/pets/views.py
@@ -1,11 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from petstagram.pets.models import Pet
+from petstagram.pets.forms import PetBaseForm, PetCreateForm
 
 
 def create_pet(request):
-    context = {}
-    return render(request, "pets/create_pet.html", context)
+    pet_form = PetBaseForm(request.POST or None)
+
+    #if request.metod == 'POST':
+    if pet_form.is_valid():
+        created_pet = pet_form.save()
+        return redirect(to='pet details', username='Diyan Kalaydzhiev', pet_slug=created_pet.slug)
+
+    context = {
+        'pet_form': pet_form,
+    }
+    return render(request, "pets/create_pet.html", context=context)
 
 
 def pet_details(request, username, pet_slug):
@@ -16,7 +26,19 @@ def pet_details(request, username, pet_slug):
 
 
 def pet_edit(request, username, pet_slug):
-    context = {}
+    pet = Pet.objects.filter(slug=pet_slug).get()
+    pet_form = PetBaseForm(request.POST or None, instance=pet)
+
+    if request.method == 'POST':
+        if pet_form.is_valid():
+            pet_form.save()
+            return redirect(to='pet details', username=username, pet_slug=pet_slug)
+
+    context = {
+        'pet_form': pet_form,
+        'username': username,
+        'pet': pet,
+    }
     return render(request, "pets/pet_edit.html", context)
 
 
