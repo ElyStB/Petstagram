8af282b142971cf30ca0e1d238b4dad88125f58d
Elena Boneva <stojcheva_eli@abv.bg>
2024-03-20 18:34:16 +0200

    add create and edit pet functionality fix pet_details.html include (with pet_photo=pet.petphoto_set) no spaces

--- petstagram/accounts/models.py
+++ petstagram/accounts/models.py
@@ -1,3 +1,56 @@
+from django.contrib.auth import models as auth_models, get_user_model
 from django.db import models
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+# auth_models.AbstractUser
+class PetstagramUser(auth_models.AbstractBaseUser, auth_models.PermissionsMixin):
+
+    email = models.EmailField(
+        _("email address"),
+        unique=True,
+        null=False,
+        blank=False,
+        error_messages={
+            "unique": _("A user with that email already exists."),
+        },
+
+    )
+
+    is_staff = models.BooleanField(
+        _("staff status"),
+        default=False,
+        help_text=_("Designates whether the user can log into this admin site."),
+    )
+    is_active = models.BooleanField(
+        _("active"),
+        default=True,
+        help_text=_(
+            "Designates whether this user should be treated as active. "
+            "Unselect this instead of deleting accounts."
+        ),
+    )
+    date_joined = models.DateTimeField(_("date joined"), default=timezone.now)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+
+UserModel = get_user_model()
+
+
+class Profile(models.Model):
+    FIRST_NAME_MAX_LENGTH = 30
+    LAST_NAME_MAX_LENGTH = 30
+
+    user = models.OneToOneField(
+        UserModel,
+        on_delete=models.CASCADE,
+        primary_key=True,
+    )
+
+    first_name = models.CharField(max_length=FIRST_NAME_MAX_LENGTH, null=True, blank=True)
+    last_name = models.CharField(max_length=LAST_NAME_MAX_LENGTH,null=True, blank=True)
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_picture = models.URLField(null=True, blank=True)
